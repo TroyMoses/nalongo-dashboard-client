5b67fc9072dfb29c038e88e4ccab9dfd30d9ff63
TroyMoses <mugabimoses07@gmail.com>
2024-06-21 05:58:28 +0800

    app

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ function App() {
       const profileObj = credential ? parseJwt(credential) : null;
 
       if (profileObj) {
-        const response = await fetch("http://localhost:8080/api/v1/users", {
+        const response = await fetch("https://nalongo-dashboard-server.onrender.com/api/v1/users", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
@@ -127,7 +127,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://nalongo-dashboard-server.onrender.com/api/v1")}
           notificationProvider={useNotificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}

892d2f248633b6b016dddc4863b22fae74a79472
TroyMoses <mugabimoses07@gmail.com>
2024-06-23 11:03:05 +0800

    resources

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -11,6 +11,21 @@ import CreateLeader from "./create-leader";
 import LeaderDetails from "./leader-details";
 import EditLeader from "./edit-leader";
 
+import AllChapterDenmark from "./all-chapter-denmark";
+import CreateChapterDenmark from "./create-chapter-denmark";
+import ChapterDenmarkDetails from "./chapter-denmark-details";
+import EditChapterDenmark from "./edit-chapter-denmark";
+
+import AllChapterGermany from "./all-chapter-germany";
+import CreateChapterGermany from "./create-chapter-germany";
+import ChapterGermanyDetails from "./chapter-germany-details";
+import EditChapterGermany from "./edit-chapter-germany";
+
+import AllChapterSwitzerland from "./all-chapter-switzerland";
+import CreateChapterSwitzerland from "./create-chapter-switzerland";
+import ChapterSwitzerlandDetails from "./chapter-switzerland-details";
+import EditChapterSwitzerland from "./edit-chapter-switzerland";
+
 import Home from "./home";
 import { Login } from "./login";
 import MyProfile from "./my-profile";
@@ -26,6 +41,18 @@ export {
   CreateLeader,
   LeaderDetails,
   EditLeader,
+  AllChapterDenmark,
+  CreateChapterDenmark,
+  ChapterDenmarkDetails,
+  EditChapterDenmark,
+  AllChapterGermany,
+  CreateChapterGermany,
+  ChapterGermanyDetails,
+  EditChapterGermany,
+  AllChapterSwitzerland,
+  CreateChapterSwitzerland,
+  ChapterSwitzerlandDetails,
+  EditChapterSwitzerland,
   Home,
   Login,
   MyProfile,
